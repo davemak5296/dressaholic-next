2429dc42eedba565c65cc06f08546ff25662748b
Dave Mak <mak9652615@gmail.com>
2022-10-01 10:49:44 +0800

    add postcss&tailwindcss, .gitattribute;set eol of config files

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  singleAttributePerLine: false,
+  plugins: [
+    require("prettier-plugin-tailwindcss"),
+    require("prettier-plugin-packagejson"),
+  ],
+};
