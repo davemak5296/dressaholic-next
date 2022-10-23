a574a9d3e7dba53788ad15ab9ab1cedb78e322cd
Dave Mak <mak9652615@gmail.com>
2022-10-23 15:43:39 +0800

    create directory-item.component.ts

---
+++ src/components/DirectoryItem/directory-item.data.ts
@@ -0,0 +1,53 @@
+import { DirectoryItemType } from '../../types';
+
+const directoryItems: DirectoryItemType[] = [
+  {
+    id: 1,
+    mainCategory: 'Men',
+    title: { name: 'men-shirts', displayName: 'Shirts' },
+    imageUrl: './assets/directory-items/maincat-men-shirts.webp',
+  },
+  {
+    id: 2,
+    mainCategory: 'Men',
+    title: { name: 'men-outwear', displayName: 'Jackets & Outwear' },
+    imageUrl: './assets/directory-items/maincat-men-jackets-and-outwear.webp',
+  },
+  {
+    id: 3,
+    mainCategory: 'Men',
+    title: { name: 'men-jeans', displayName: 'Pants & Jeans' },
+    imageUrl: './assets/directory-items/maincat-men-pants-and-jeans.webp',
+  },
+  {
+    id: 4,
+    mainCategory: 'Men',
+    title: { name: 'men-activewear', displayName: 'Activewear' },
+    imageUrl: './assets/directory-items/maincat-men-activewear.webp',
+  },
+  {
+    id: 5,
+    mainCategory: 'Women',
+    title: { name: 'women-tops', displayName: 'Tops' },
+    imageUrl: './assets/directory-items/maincat-women-tops.webp',
+  },
+  {
+    id: 6,
+    mainCategory: 'Women',
+    title: { name: 'women-dresses', displayName: 'Dresses & Skirts' },
+    imageUrl: './assets/directory-items/maincat-women-dresses-and-skirts.webp',
+  },
+  {
+    id: 7,
+    mainCategory: 'Women',
+    title: { name: 'women-jeans', displayName: 'Pants & Jeans' },
+    imageUrl: './assets/directory-items/maincat-women-pants-and-jeans.webp',
+  },
+  {
+    id: 8,
+    mainCategory: 'Women',
+    title: { name: 'women-activewear', displayName: 'Activewear' },
+    imageUrl: './assets/directory-items/maincat-women-activewear.webp',
+  },
+];
+export default directoryItems;
