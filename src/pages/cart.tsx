2d3276d514303e806521485c09ca87d4708a3330
Dave Mak <mak9652615@gmail.com>
2022-11-07 00:10:01 +0800

    del comments

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import clsx from 'clsx';
-import { ReactComponent as MinusSign } from '../assets/circle-minus-solid.svg';
-import { ReactComponent as PlusSign } from '../assets/circle-plus-solid.svg';
-import { ReactComponent as CrossSign } from '../assets/circle-xmark-solid.svg';
 import Footer from '../components/footer';
 import useFooterFixed from '../hooks/useFooterFixed';
 import { useSelector } from 'react-redux';
@@ -37,48 +34,6 @@ const Cart: React.FC = () => {
           <div className={colTitleStyles}>Description</div>
         </section>
         {itemsInCart && itemsInCart.map((item, index) => <CartItem key={index} item={item} />)}
-        {/* cart item - when smaller than sm */}
-        {/* <section className="grid grid-cols-cart text-xs xs:text-sm sm:hidden">
-          <div className="col-span-1 col-start-1 row-span-2 row-start-1 flex justify-center p-2">
-            <img className="w-2/3" src="./men-jacket-1.webp" alt="" />
-          </div>
-          <div className="col-span-3 col-start-2 row-span-1 row-start-1 p-2 xs:pt-4">
-            Kirkland Signature Men&apos;s Jacket Kirkland Signature Men&apos;s Jacket
-            <div className="text-xs">&nbsp;&nbsp;color: </div>
-            <div className="text-xs">&nbsp;&nbsp;size: </div>
-          </div>
-          <div className="col-span-1 col-start-2 row-span-1 row-start-2 flex justify-center">
-            $43.98
-          </div>
-          <div className="col-span-1 col-start-3 row-span-1 row-start-2 flex justify-center">
-            <MinusSign className="h-4 w-4 cursor-pointer">-</MinusSign>
-            <div className="ml-1">1</div>
-            <PlusSign className="ml-1 h-4 w-4 cursor-pointer">-</PlusSign>
-          </div>
-          <div className="col-span-1 col-start-4 row-span-1 row-start-2 flex justify-center font-bold">
-            $43.98
-          </div>
-        </section> */}
-        {/* cart item - when larger than sm */}
-        {/* <section className="hidden grid-cols-cartXL items-center text-sm sm:grid lg:text-base xl:text-lg">
-          <div className="p-4">
-            <img className="w-2/3" src="./men-jacket-1.webp" alt="" />
-          </div>
-          <div className="w-4/5">
-            Kirkland Signature Men&apos;s Jacket Kirkland Signature Men&apos;s Jacket
-            <div className="text-xs lg:text-sm xl:text-base">&nbsp;&nbsp;color: </div>
-            <div className="text-xs lg:text-sm xl:text-base">&nbsp;&nbsp;size: </div>
-          </div>
-          <div>$43.98</div>
-          <div className="flex items-center">
-            <MinusSign className="h-4 w-4 cursor-pointer" />
-            <div className="ml-1 text-lg">1</div>
-            <PlusSign className="ml-1 h-4 w-4 cursor-pointer" />
-          </div>
-          <div>$43.98</div>
-          <CrossSign className="flex h-5 w-5 justify-center" />
-        </section> */}
-        <hr />
       </main>
       <Footer isFixed={isFooterFixed} />
     </>
--- src/store/cart/cart.action.ts
+++ src/store/cart/cart.action.ts
@@ -1,59 +1,7 @@
 import { CartItemType } from '../../types';
 import { SET_CART_ITEMS } from './cart.reducer';
 import produce from 'immer';
-import { selectCartItems } from './cart.selector';
 import { WritableDraft } from 'immer/dist/internal';
-// import _ from 'lodash';
-
-// helper function to change quantity
-// const changeQty = (
-//   cartItems: CartItem[],
-//   targetProductOrItem: CartItem | Product,
-//   operation: 'add' | 'deduct'
-// ) => {
-//   if (operation === 'add') {
-//     return cartItems.map((cartItem) =>
-//       cartItem.id === targetProductOrItem.id ? { ...cartItem, qty: cartItem.qty + 1 } : cartItem
-//     );
-//   } else {
-//     return cartItems.map((cartItem) =>
-//       cartItem.id === targetProductOrItem.id ? { ...cartItem, qty: cartItem.qty - 1 } : cartItem
-//     );
-//   }
-// };
-
-// helper function to "addItemToCart"
-// const addCartItem = (cartItems: CartItem[], productToAdd: Product) => {
-//   const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
-
-//   return existingCartItem
-//     ? changeQty(cartItems, productToAdd, 'add')
-//     : [...cartItems, { ...productToAdd, qty: 1 }];
-// };
-// helper functoin to "removeItemFromCart"
-// const removeCartItem = (cartItems: CartItem[], itemToRemove: CartItem) => {
-//   const existingCartItem = cartItems.find((cartItem) => cartItem.id === itemToRemove.id);
-
-//   return existingCartItem?.qty === 1
-//     ? clearCartItem(cartItems, itemToRemove)
-//     : changeQty(cartItems, itemToRemove, 'deduct');
-// };
-// helper function to "removeItemInCart"
-// const clearCartItem = (cartItems: CartItem[], itemToClear: CartItem) =>
-//   cartItems.filter((cartItem) => cartItem.id !== itemToClear.id);
-
-// export const addItemToCart = (cartItems: CartItem[], productToAdd: Product) => {
-//   const newCartItems = addCartItem(cartItems, productToAdd);
-//   return SET_CART_ITEMS(newCartItems);
-// };
-// export const removeItemFromCart = (cartItems: CartItem[], itemToRemove: CartItem) => {
-//   const newCartItems = removeCartItem(cartItems, itemToRemove);
-//   return SET_CART_ITEMS(newCartItems);
-// };
-// export const clearItemInCart = (cartItems: CartItem[], itemToClear: CartItem) => {
-//   const newCartItems = clearCartItem(cartItems, itemToClear);
-//   return SET_CART_ITEMS(newCartItems);
-// };
 
 const indexInCart = (draft: WritableDraft<CartItemType[]>, itemToChange: CartItemType) => {
   const index = draft.findIndex((existItem) => {
