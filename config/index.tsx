2017c61388bcd366af6ca9fa62552ac6ff8d67aa
Dave Mak <mak9652615@gmail.com>
2023-05-25 20:56:15 +0800

    update production url in config/index.tsx

--- config/index.tsx
+++ config/index.tsx
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://dressaholic-next-od0s78zm4-davemak5296.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://dressaholic-next.vercel.app'
